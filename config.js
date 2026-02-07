3ede237d6f3368daa3c0a34085097a437e2b3cf2
ajayajay293 <cmurah60@gmail.com>
2026-02-07 19:19:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     // API Layanan SMM (FayuPedia)
     BASE_URL: 'https://fayupedia.id/api',
     API_ID: '87026',
-    API_KEY: 'ujjfjw-sk0uzp-uhofak-ymrvna-hatk89',
+    API_KEY: 'dreog8-m00rv0-qvfkut-sssvip-tjg1kz',
 
     // API Deposit (RumahOTP)
     atlanticApiKey: 'cIr6yFSfNiCtzfOw50IIb8xvviGlG4U9o7wLe60Pvrz9os0Ff0ARoAMKdNj7YyqVYi25YtfQoyGVlPo8ce3wAuawklZJlqJF6mmN',
